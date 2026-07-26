2d55e70fc1358c1b261f8fd99a1a0c2442d194f4
lucy wonsower <lucywonsower@lucys-MacBook-Pro-2.local>
2026-07-25 19:28:13 -0700

    update nextjs config again

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? "/website" : "",
-  assetPrefix: isProd ? "/website/" : "",
 };
 
 export default nextConfig;
